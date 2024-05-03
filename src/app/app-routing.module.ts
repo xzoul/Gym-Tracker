176c3005199119b1058bbfb5f55bb67fbfaf3634
Xzoul <jimmyprima.delacruzjr@bicol-u.edu.ph>
2024-05-03 17:13:12 +0800

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,32 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+    {
+        path: "",
+        loadChildren: () =>
+            import("./tabs/tabs.module").then((m) => m.TabsPageModule),
+        // loadChildren: () =>
+        //     import("./views/loader/loader.module").then(
+        //         (m) => m.LoaderPageModule
+        //     ),
+    },
+    {
+        path: "loader",
+        loadChildren: () =>
+            import("./views/loader/loader.module").then(
+                (m) => m.LoaderPageModule
+            ),
+    },  {
+    path: 'index',
+    loadChildren: () => import('./views/index/index.module').then( m => m.IndexPageModule)
+  },
+
 ];
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+    ],
+    exports: [RouterModule],
 })
 export class AppRoutingModule {}
